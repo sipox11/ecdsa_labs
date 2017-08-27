9b92bfabff7e5e345298c33f0e71bb6bd397c233
Jose Carlos <sipox11@gmail.com>
2017-08-27 12:35:06 +0200

    Prepare skeleton for signing script

--- sign_messages.rb
+++ sign_messages.rb
@@ -5,9 +5,18 @@ MSG_DIR = "./messages"
 
 def show_help_msg
 	puts "\nUSAGE:\nruby sign_messages.rb <msg1> <msg2> <msg3> ..."
-	puts "	- <msg1> is the filename of a message stored in #{SIGN_DIR}"
+	puts "	- <msg1> is a string containing the message"
 	puts "Description: Signs an arbitrary number of messages using ECDSA."
 	puts "Output: Stores the signatures under the #{SIGN_DIR}\n\n"
+end
+
+def store_msg(counter, msg)
+	msg_file = File.new("#{MSG_DIR}/msg_#{counter}", "w+")
+	msg_file.write("#{msg}")
+end
+
+def sign_msg(counter, msg)
+
 end
 
 system "clear"
@@ -16,13 +25,15 @@ if num_of_messages == 0
 	show_help_msg
 else
 	for i in 0..(ARGV.size-1)
-		message = File.file?(ARGV[i]) ? File.read("#{MSG_DIR}/#{ARGV[i]}") : ""
+		message = ARGV[i]
 		msg_size = message.size
 		puts "================================================"
 		puts "\nMessage ##{i} [SIZE=#{msg_size}]: #{message}"
 		if msg_size > 0
-			# sign_message(message, "#{ARGV[i]}.sig")
-			puts "Signing message"
+			# Store message and sign it
+			store_msg(i, message)
+			sign_msg(i, message)
+			puts "Signing message...\n"
 		else
 			puts "Message size is 0"
 		end
