6d71c609570d852ee83cfa0a802f33d106f73543
Jose Carlos <sipox11@gmail.com>
2017-08-27 12:17:16 +0200

    Accept valid arguments to sign

--- sign_messages.rb
+++ sign_messages.rb
@@ -1,16 +1,35 @@
 require 'digest/sha1'
 
 SIGN_DIR = "./signatures"
+MSG_DIR = "./messages"
 
 def show_help_msg
-	puts "\nUSAGE:\n sign_messages <msg1> <msg2> <msg3> ..."
+	puts "\nUSAGE:\nruby sign_messages.rb <msg1> <msg2> <msg3> ..."
+	puts "	- <msg1> is the filename of a message stored in #{SIGN_DIR}"
 	puts "Description: Signs an arbitrary number of messages using ECDSA."
-	puts "Output: Stores the signatures under the #{SIGN_DIR}"
+	puts "Output: Stores the signatures under the #{SIGN_DIR}\n\n"
 end
 
+system "clear"
 num_of_messages = ARGV.size
-show_help_msg if num_of_messages == 0
-
+if num_of_messages == 0
+	show_help_msg
+else
+	for i in 0..(ARGV.size-1)
+		message = File.file?(ARGV[i]) ? File.read("#{MSG_DIR}/#{ARGV[i]}") : ""
+		msg_size = message.size
+		puts "================================================"
+		puts "\nMessage ##{i} [SIZE=#{msg_size}]: #{message}"
+		if msg_size > 0
+			# sign_message(message, "#{ARGV[i]}.sig")
+			puts "Signing message"
+		else
+			puts "Message size is 0"
+		end
+		puts "\n"
+	end
+end
+puts "================================================"
 
 
 
